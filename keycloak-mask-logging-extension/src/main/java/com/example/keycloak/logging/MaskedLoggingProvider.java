15be15b9bd471d2b6f6e1b18c3a449d87d1ec39b
Andres David Niño <deivnini@gmail.com>
2024-09-18 15:37:24 -0500

    2nd  attempt

--- keycloak-mask-logging-extension/src/main/java/com/example/keycloak/logging/MaskedLoggingProvider.java
+++ keycloak-mask-logging-extension/src/main/java/com/example/keycloak/logging/MaskedLoggingProvider.java
@@ -5,30 +5,29 @@ import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventListenerProviderFactory;
 import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.events.admin.OperationType;
+import org.keycloak.events.admin.ResourceType;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.jboss.logging.Logger;
 
 public class MaskedLoggingProvider implements EventListenerProvider, EventListenerProviderFactory {
-
     private static final Logger logger = Logger.getLogger(MaskedLoggingProvider.class);
 
     @Override
     public void onEvent(Event event) {
-        if (event.getType() == EventType.LOGIN_ERROR) {
+        if (event.getType() == EventType.LOGIN_ERROR || event.getType() == EventType.LOGIN) {
             String maskedUser = maskUsername(event.getUserId());
-            logger.infof("Failed login attempt for user: %s", maskedUser);
+            logger.infof("Login attempt for user: %s", maskedUser);
         }
     }
 
     @Override
-    public void onEvent(AdminEvent adminEvent, boolean b) {
-        /*
-        if (adminEvent.getOperationType() == AdminEvent.OperationType.ACTION && adminEvent.getResourceType() == AdminEvent.ResourceType.USER) {
+    public void onEvent(AdminEvent adminEvent, boolean includeRepresentation) {
+        if (adminEvent.getOperationType() == OperationType.ACTION && adminEvent.getResourceType() == ResourceType.USER) {
             String maskedUser = maskUsername(adminEvent.getResourcePath());
             logger.infof("Admin action performed on user: %s", maskedUser);
         }
-        */
     }
 
     private String maskUsername(String username) {
@@ -58,7 +57,8 @@ public class MaskedLoggingProvider implements EventListenerProvider, EventListen
     @Override
     public void close() {}
 
-    public MaskedLoggingProvider create(KeycloakSession session) {
+    @Override
+    public EventListenerProvider create(KeycloakSession session) {
         return new MaskedLoggingProvider();
     }
 
